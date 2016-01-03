ed857573d49166a531ac84acf97ef40fe4865eb6
tangledfruit <code@tangledfruit.com>
2016-01-03 00:27:31 -0800

    Add .createDatabase method.

--- test/testServer.js
+++ test/testServer.js
@@ -3,6 +3,7 @@
 const Rx = require('rx');
 const rxCouch = require('../lib/server');
 const expect = require('chai').expect;
+const nock = require('nock');
 
 //------------------------------------------------------------------------------
 
@@ -29,6 +30,26 @@ const expectOneResult = function (observable, done, match) {
       done(err);
     },
 
+    function () {
+      done(failure);
+    });
+
+};
+
+//------------------------------------------------------------------------------
+
+const expectNoResults = function (observable, done) {
+
+  var failure;
+
+  observable.subscribe(
+    function (value) {
+      if (!failure)
+        failure = new Error("Unexpected value: ", value);
+    },
+    function (err) {
+      done(err);
+    },
     function () {
       done(failure);
     });
@@ -133,5 +154,38 @@ describe('rx-couch', function () {
     });
 
   });
+
+  //----------------------------------------------------------------------------
+
+  describe('.createDatabase()', function () {
+
+    it('should return an Observable which sends only onCompleted when done', function (done) {
+      expectNoResults(server.createDatabase('text-rx-couch'), done);
+    });
+
+    it('should succeed even if the database already exists', function (done) {
+      expectNoResults(server.createDatabase('text-rx-couch'), done);
+    });
+
+    it('should throw if database name is missing', function () {
+      expect(() => server.createDatabase()).to.throw();
+    });
+
+    it('should throw if database name is empty', function () {
+      expect(() => server.createDatabase("")).to.throw();
+    });
+
+    it('should send an onError message if server yields unexpected result', function (done) {
+
+      nock('http://localhost:5984')
+        .post('/test-rx-couch')
+        .reply(500, "Server blew up");
+
+      expectOnlyError(server.createDatabase('text-rx-couch'), done);
+
+    });
+
+  });
+
 
 });
